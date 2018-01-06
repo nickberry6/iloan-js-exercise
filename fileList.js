ae21a5524ea871f14f8a64c27dd7c74aa1534664
Nick Berry <nicholasberry.13@gmail.com>
2018-01-06 13:48:29 -0500

    refactoring out repetitions and styling

--- fileList.js
+++ fileList.js
@@ -10,14 +10,14 @@ function activate() {
   renderList();
 }
 
-function addFile(evt) {
-  files.push(evt.target.files[0]);
-  totalSize += evt.target.files[0].size;
+function addFile(ev) {
+  files.push(ev.target.files[0]);
+  totalSize += ev.target.files[0].size;
   renderList();
 }
 
-function removeFile(evt) {
-  var idxOfDel = evt.target.id;
+function removeFile(ev) {
+  var idxOfDel = ev.target.id;
   totalSize -= files[idxOfDel].size;
   files.splice(idxOfDel, 1);
   renderList();
@@ -26,10 +26,12 @@ function removeFile(evt) {
 function renderList() {
   var list = [];
   for(var i = 0; i < files.length; i++) {
-    var buttonHTML = '<br><button onclick="removeFile(event)" id="' + i + '">Delete</button>';
-    var itemHTML = '<div class="tile">' + files[i].name + '<br>' + files[i].type + 
-        '<br>' + files[i].size + buttonHTML + '</div>';
-    list.push(itemHTML);
+    var buttonHTML = '<br><button onclick="removeFile(event)" class="delete" id="' + i + '">Delete</button>',
+        detailHTML = '<span>Name: </span>' + files[i].name + '<br>' +
+                     '<span>File type: </span>' + files[i].type + '<br>' +
+                     '<span>File size: </span>' + files[i].size + ' bytes';
+    var tileHTML = '<div class="tile">' + detailHTML + buttonHTML + '</div>';
+    list.push(tileHTML);
   }
   document.getElementById('list').innerHTML = list.join(" ");
   document.getElementById('total').innerHTML = 'Total file size: ' + totalSize + ' bytes';
